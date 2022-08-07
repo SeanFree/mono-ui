ce164a9e2d55859816362a9b7a805b5bf512af90
Sean Free <seanalexanderfree@gmail.com>
2022-08-06 22:41:29 -0400

    Updated component exports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as Button } from './Button'
 export { default as Checkbox } from './Checkbox'
+export { default as CollapsibleSection } from './CollapsibleSection'
 export { default as Heading } from './Heading'
 export { default as Icon } from './Icon'
 export { default as IconButton } from './IconButton'
